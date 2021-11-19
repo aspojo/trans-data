6d2ca4b0ddd67831952078651a59de7ed7c9cfec
aspojo <logbug@aliyun.com>
2021-11-19 11:58:23 +0800

    Initial commit

---
+++ src/main/java/top/logbug/transdata/TransDataApplication.java
@@ -0,0 +1,13 @@
+package top.logbug.transdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/logbug/transdata/TransDataApplicationTests.java
@@ -0,0 +1,13 @@
+package top.logbug.transdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
